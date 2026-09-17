1cb63eaf462c8b5842f6f646d6cad95b4b8e5c08
danielbendavid <danielbendavid776@gamil.com>
2026-09-17 19:38:47 +0300

    .

--- targil_3.py
+++ targil_3.py
@@ -1,4 +1,18 @@
 def camel_to_hyphen(text):
-    return text.replace(text.upper(), text.lower())
+    '''
+    returns a string from camel case to hyphen case
+    :param text: string
+    :return: str from camel case to hyphen case with
+    '''
+    result = str()
+    for c in text:
+        if c.isupper():
+            result += '-' + c.lower()
+        else:
+            result += c
+    return result
 print(camel_to_hyphen('helloPython'))
+print(camel_to_hyphen('myVariableName'))
+print(camel_to_hyphen('python'))
+print(camel_to_hyphen('aBigTest'))
 
