9dcb4c43af2bb1a460d8ca74bf2b5b214c3e318a
danielbendavid <danielbendavid776@gamil.com>
2026-09-17 18:57:24 +0300

    .

---
+++ targil_1.py
@@ -0,0 +1,24 @@
+valid_ranks = []
+while True:
+    try:
+        rank = int(input('enter rank: '))
+        if rank == -999 and len(valid_ranks) >= 10:
+            break
+        if rank == -999 and len(valid_ranks) < 10:
+            print('need at least 10 valid ranks, keep entering')
+        elif rank < 1 or rank > 5:
+            print('not in range, skip')
+        else:
+            valid_ranks.append(rank)
+    except ValueError:
+        print('invalid input, skip')
+
+
+
+average = sum(valid_ranks) / len(valid_ranks)
+highest = max(valid_ranks)
+
+print(f'number of valid ranks: {len(valid_ranks)}')
+print(f'average rank: {average:.2f}')
+print(f'highest rank :{highest}')
+
